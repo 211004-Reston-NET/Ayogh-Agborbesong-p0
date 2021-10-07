c8f031a980ece4e8453d48c807ffbe38a7761305
Ayogh <aayogh@yahoo.com>
2021-10-07 02:29:21 -0400

    first commit

---
+++ RestModels/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestModels
+{
+    public class Restaurant
+    {
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Name { get; set; }
+        public string Menus { get; set; }
+    }
+}
